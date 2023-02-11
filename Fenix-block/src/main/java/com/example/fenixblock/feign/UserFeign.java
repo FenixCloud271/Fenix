89e91f8077dabb1ff369774be6a736ba7aba41c7
CloudActiv <1793879055@qq.com>
2023-02-11 22:54:16 +0800

    test

--- Fenix-block/src/main/java/com/example/fenixblock/feign/UserFeign.java
+++ Fenix-block/src/main/java/com/example/fenixblock/feign/UserFeign.java
@@ -5,8 +5,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import utils.Result;
 
-import java.util.List;
-
 @FeignClient(name = "Fenix-user")
 public interface UserFeign {
     @GetMapping("/userTx/getAddressById")
